df715204dff683de809089cf88574fe77b83c396
Claudio M. Llanca <claudioll@gmail.com>
2021-08-19 03:25:09 +0100

    new working version plant_watering.py

--- plant_watering.py
+++ plant_watering.py
@@ -1,36 +1,46 @@
+import logging
 import schedule
 import time
 import RPi.GPIO as GPIO
 
+logging.basicConfig(filename='/var/log/plants_watering.log', level=logging.DEBUG)
+
 GPIO.setmode(GPIO.BCM)  # GPIO Numbers instead of board numbers
 
 RELAYS = [17, 27]
+WATERING_TIME = 3
 
 
 def initialize_relays():
     for relay in RELAYS:
+        logging.info("configuring relay %s as OUT mode" % str(relay))
         GPIO.setup(relay, GPIO.OUT)  # GPIO Assign mode
-        GPIO.output(relay, GPIO.LOW)
+        logging.info("setting default state for relay %s to LOW" % str(relay))
+        GPIO.output(relay, GPIO.LOW) # Default state
 
 
-def job():
+def watering_job():
     for relay in RELAYS:
-        print("+ activating %s" % str(relay))
+        logging.info("activating relay %s" % str(relay))
         GPIO.output(relay, GPIO.HIGH)
-        time.sleep(3)
-        print("- de-activating %s" % str(relay))
+        time.sleep(WATERING_TIME)
+        logging.info("de-activating relay %s" % str(relay))
         GPIO.output(relay, GPIO.LOW)
 
-
-schedule.every(10).seconds.do(job)
 
 try:
+    initialize_relays()
+    #schedule.every(10).seconds.do(watering_job)
+    schedule.every().day.at("21:00").do(watering_job)
     while True:
         schedule.run_pending()
         time.sleep(1)
 
-except Exception:
-    pass
+except KeyboardInterrupt:
+    logging.warning("program interrupted")
+
+except Exception as e:
+    logging.exception("%s" % str(e))
 
 finally:
     GPIO.cleanup()
